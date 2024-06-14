e249e8242ac5523a741bc80d0edd66e6e2647d97
Nuril Kaunaini R. <31752461+amseels@users.noreply.github.com>
2024-06-14 05:07:13 +0000

    first innit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,35 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import altair as alt
+from streamlit_gsheets import GSheetsConnection
 
-# Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+# Title of the app
+st.title('📗 Koleksi Teh Iin')
+st.write("#### organized by 1000 Lentera")
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
-  
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+url = "https://docs.google.com/spreadsheets/d/1ERvukhKnlhVBA3-0SIX8hc-gMt0NWk3tW-3QJoR75Js/edit?gid=0#gid=0"
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+books = conn.read(spreadsheet=url)
 
-# Load data
-df = pd.read_csv('data/movies_genres_summary.csv')
-df.year = df.year.astype('int')
+sort_options = ['Judul', 'Penulis']
+sort_by = st.selectbox('Sort by', sort_options)
+sorted_books = books.sort_values(by=sort_by)
 
-# Input widgets
-## Genres selection
-genres_list = df.genre.unique()
-genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+# st.dataframe(books)
 
-## Year selection
-year_list = df.year.unique()
-year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
+tab1, tab2 = st.tabs(["Seluruh Koleksi", "Cari Buku"])
 
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
+# Gallery view
+with tab1:
+  st.write("### Book List")
+  st.dataframe(sorted_books[['Judul', 'Penulis', 'Kategori']])  # Display book titles and authors in a table
 
 
-# Display DataFrame
+# Book details view
+with tab2:
+    selected_book_title = st.selectbox("Cari Judul Buku", sorted_books['Judul'], index = None)
 
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-                            column_config={"year": st.column_config.TextColumn("Year")},
-                            num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
-
-# Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-            x=alt.X('year:N', title='Year'),
-            y=alt.Y('gross:Q', title='Gross earnings ($)'),
-            color='genre:N'
-            ).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
+    if selected_book_title is not None:
+      book = books[books['Judul'] == selected_book_title].iloc[0]
+      # st.image(book['Foto'], width=200)
+      st.subheader(book['Judul'])
+      st.write(f"**Author:** {book['Penulis']}")
+      st.write(book['Deskripsi'])
